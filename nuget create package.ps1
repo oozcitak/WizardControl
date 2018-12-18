8b6a5b43ec8b67f3c41abe3df8fad68289e6449d
Ozgur Ozcitak <ozcitak@yahoo.com>
2018-12-18 13:18:04 +0300

    Use absolute paths in publish script.

--- nuget create package.ps1
+++ nuget create package.ps1
@@ -1,3 +1,8 @@
+# directory containing this script
+$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$ProjDir  = Join-Path -Path $ScriptDir -ChildPath "WizardControl\"
+$ProjFile  = Join-Path -Path $ScriptDir -ChildPath "WizardControl\WizardControl.csproj"
+
 # download latest nuget.exe
 $nugeturl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 $nugetexe = "$($env:temp)\nuget.exe"
@@ -5,5 +10,4 @@ $client = new-object System.Net.WebClient
 $client.DownloadFile($nugeturl, $nugetexe)
 
 # create the nuspec file for the "Release" build
-Invoke-Expression "$($nugetexe) Pack '.\WizardControl\WizardControl.csproj' -Properties Configuration=Release"
-
+Invoke-Expression "$($nugetexe) Pack '$($ProjFile)' -OutputDirectory '$($ProjDir)' -Properties Configuration=Release"
